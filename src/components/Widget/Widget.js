41d8db4aa5257b837ab6b2e6d87f9772e6a7df32
pseudozach <zozsoy@gmail.com>
2021-10-18 23:33:08 -0700

    got it working!

--- src/components/Widget/Widget.js
+++ src/components/Widget/Widget.js
@@ -18,6 +18,9 @@ import LoadingButton from '@mui/lab/LoadingButton';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import SendIcon from '@mui/icons-material/Send';
 import LockIcon from '@mui/icons-material/Lock';
+import CancelIcon from '@mui/icons-material/Cancel';
+import Tooltip from '@mui/material/Tooltip';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 import { StacksTestnet, StacksMocknet, StacksMainnet } from '@stacks/network';
 import { openContractCall } from '@stacks/connect';
@@ -40,21 +43,21 @@ import {
 import bigInt from 'big-integer';
 import { BN } from 'bn.js';
 
-let mocknet = new StacksMocknet();
+let mocknet = new StacksMocknet({url: 'https://3999-azure-cricket-glgzsgrt.ws-us17.gitpod.io'});
 // mocknet.coreApiUrl = 'http://localhost:3999';
+// mocknet.coreApiUrl = 'https://3999-azure-cricket-glgzsgrt.ws-us17.gitpod.io'
 const testnet = new StacksTestnet();
 const mainnet = new StacksMainnet();
 let activeNetwork = mocknet
 
-let stacksNetworkType = "mocknet";
-
-if(stacksNetworkType==="mocknet"){
-  activeNetwork = mocknet
-} else if(stacksNetworkType==="testnet"){
-  activeNetwork = testnet
-} else if(stacksNetworkType==="mainnet"){
-  activeNetwork = mainnet
-}
+// let stacksNetworkType = "mocknet";
+// if(stacksNetworkType==="mocknet"){
+//   activeNetwork = mocknet
+// } else if(stacksNetworkType==="testnet"){
+//   activeNetwork = testnet
+// } else if(stacksNetworkType==="mainnet"){
+//   activeNetwork = mainnet
+// }
 
 const widgetName = Config.name;
 const apiUrl = Config.apiUrl;
@@ -68,12 +71,12 @@ const customStyles = {
       bottom: 'auto',
       marginRight: '-50%',
       transform: 'translate(-50%, -50%)',
-    },
+    }
   };
 
   const style = {
     position: 'absolute',
-    top: '50%',
+    top: '40%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: 400,
@@ -87,7 +90,17 @@ const customStyles = {
   const nounderline = {
       textDecoration: "none",
   }
-  
+
+  const scrollModalStyle = {
+    position:'absolute',
+    top:'10%',
+    left:'10%',
+    overflow:'scroll',
+    height:'100%',
+    display:'block',
+    maxHeight: '90%',
+  }
+
   const randomBytes = size => {
     const bytes = Buffer.allocUnsafe(size);
     global.crypto.getRandomValues(bytes);
@@ -99,12 +112,6 @@ class Widget extends React.Component {
     constructor(props) {
         super(props);
         
-        // generate one-time-use preimage/preimageHash
-        const preimage = randomBytes(32);
-        // console.log(`preimage: ${preimage.toString('hex')}`);
-        const preimageHash = crypto.sha256(preimage);
-        // console.log(`preimageHash: ${preimageHash}`);
-
         this.state = {
             message: [],
             modalIsOpen: false,
@@ -112,8 +119,8 @@ class Widget extends React.Component {
             stxAmount: '10',
             invoiceAmount: 0,
             invoiceAmountBTC: '...',
-            preimage: preimage.toString('hex'),
-            preimageHash: preimageHash.toString('hex'),
+            preimage: '',
+            preimageHash: '',
             rate: '30000',
             fee: '5',
             paymentLink: 'invoice',
@@ -129,6 +136,7 @@ class Widget extends React.Component {
             showQr: true,
             buttonLoading: false,
             showComplete: false,
+            showCopyTooltip: false,
         };
     }
 
@@ -166,6 +174,7 @@ class Widget extends React.Component {
                 onClose={this.handleClose}
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
+                style={scrollModalStyle}
                 >
                     <Box sx={style}>
                         <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ textAlign: 'center' }}>
@@ -251,13 +260,36 @@ class Widget extends React.Component {
                                 textAlign: 'center',
                             }}
                             >
-                            {this.state.showQr ? (<a href={this.state.paymentLink}>
+                            {this.state.showQr ? (
+                                <>
+                                <a href={this.state.paymentLink}>
                                 <QRCode 
                                     value={this.state.invoice} 
-                                />
-                            </a>) : <LockIcon color="theme.palette.success.dark" fontSize="large" sx={{ fontSize: '10em'}} />}
+                                /></a>
+                                <Box sx={{ my: '1em', cursor: 'pointer'}} fullWidth>
+                                    <Tooltip open={this.state.showCopyTooltip} title="Copied">
+                                        <TextField 
+                                            disabled 
+                                            fullWidth
+                                            size="small"
+                                            variant="outlined" 
+                                            value={this.state.invoice} 
+                                            maxRows={1} 
+                                            onClick={this.copyToClipboard}
+                                            sx={{ cursor: 'pointer'}}
+                                            InputProps={{
+                                                endAdornment: <InputAdornment position="end"><ContentCopyIcon /></InputAdornment>,
+                                                style: { cursor: 'pointer' }
+                                            }}
+                                        />
+                                    </Tooltip>
+                                </Box>
+                                </>
+                            ) : null}
+                            {this.state.swapStatus.includes("lock") ? (<LockIcon color="theme.palette.success.dark" fontSize="large" sx={{ fontSize: '5em'}} />) : null}
+                            {this.state.swapStatus.includes("fail") ? (<CancelIcon color="theme.palette.error.dark" fontSize="large" sx={{ fontSize: '5em'}} />) : null}
                             {this.state.showComplete ? (
-                                <CheckCircleIcon color="success" fontSize="large" sx={{ fontSize: '10em'}} />
+                                <CheckCircleIcon color="success" fontSize="large" sx={{ fontSize: '5em'}} />
                             ) : null}
                             {this.state.showStatus ? (
                                 <Typography variant="body1" gutterBottom component="div" sx={{ mx: 'auto', textAlign: 'center' }} color={this.state.statusColor}>
@@ -271,13 +303,14 @@ class Widget extends React.Component {
                             <Box
                                 sx={{mx: 'auto',textAlign: 'center',}}>
                                 <LoadingButton
-                                    sx={{ background: '#263238', mx: 'auto'}}
+                                    sx={{ mx: 'auto'}}
                                     onClick={this.claimStx}
                                     endIcon={<SendIcon />}
                                     loading={this.state.buttonLoading}
                                     loadingPosition="end"
                                     variant="contained"
                                     size="large"
+                                    color="secondary"
                                 >
                                 {this.state.buttonText}
                                 </LoadingButton>
@@ -327,6 +360,14 @@ class Widget extends React.Component {
     //     this.getpairs();
     // }
 
+    copyToClipboard = () => {
+        let thisthing = this;
+        navigator.clipboard.writeText(this.state.invoice);
+        this.setState({showCopyTooltip: true});
+        setTimeout(() => {
+            thisthing.setState({showCopyTooltip: false});
+        }, 1000);
+    }
     handleClick = () => {
         this.setState({buttonLoading: true,});
     }
@@ -334,8 +375,17 @@ class Widget extends React.Component {
         console.log(`closeModal `, this.state);
         this.setState({modalIsOpen: false});
     }
+    createSecret = () => {
+        // generate one-time-use preimage/preimageHash
+        const preimage = randomBytes(32);
+        // console.log(`preimage: ${preimage.toString('hex')}`);
+        const preimageHash = crypto.sha256(preimage);
+        // console.log(`preimageHash: ${preimageHash}`);
+        this.setState({preimage: preimage.toString('hex'), preimageHash: preimageHash.toString('hex')});
+        this.createswap();
+    }
     setMessage(...message){
-        console.log(`swapParams: `, message)
+        // console.log(`swapParams: `, message)
         this.setState({claimAddress: message[0], stxAmount: message[1], modalIsOpen: true});
         this.getpairs();
     }
@@ -349,25 +399,26 @@ class Widget extends React.Component {
             },
             }).then(res => res.json())
             .then(res => {
-                console.log("getpairs: ", res);
+                // console.log("getpairs: ", res);
                 const rate = parseInt(res.pairs[pairId].rate);
                 const maxLimit = res.pairs[pairId].limits.maximal;
                 const minLimit = res.pairs[pairId].limits.minimal;
                 const fee = res.pairs[pairId].fees.percentage;
-                const invoiceAmount = parseInt((this.state.stxAmount / rate) * 100000000 * (1+(fee/100)) );
-                const invoiceAmountBTC = (this.state.stxAmount / rate).toFixed(8).toString();
+                const targetStxAmount = (100*this.state.stxAmount)/(100-fee);
+                // const invoiceAmount = parseInt((this.state.stxAmount / rate) * 100000000 * (1+(fee/100)) );
+                const invoiceAmount = Math.ceil((targetStxAmount / rate) * 100000000);
+                const invoiceAmountBTC = (invoiceAmount / 100000000).toFixed(8).toString();
                 // console.log(`setting fee, rate ${fee}, ${rate}, ${this.state.stxAmount}, ${invoiceAmount}, ${invoiceAmountBTC}`);
                 
                 // convert rate, populate UI
                 this.setState({rate, fee, invoiceAmount, invoiceAmountBTC});
                 
-                // create swap and show LN invoice
-                this.createswap();
-
-                // $("#minted").text(parseInt(res.result.slice(-2), 16));
+                // create preimage and then swap and show LN invoice
+                this.createSecret();
             })
             .catch(e => {
                 console.log(`getpairs error `, e);
+                this.setState({showLoading: false, showStatus: true, swapStatus: 'Unable to reach LNSwap. Please try again later.', statusColor: 'error', showQr: false});
                 // return e;
             });     
     }
@@ -390,21 +441,27 @@ class Widget extends React.Component {
             body: JSON.stringify(reqbody)
             }).then(res => res.json())
             .then(res => {
-                console.log("swap created: ", res);
+                // console.log("swap created: ", res);
+                if(res.error) {
+                    this.setState({showLoading: false, showStatus: true, swapStatus: 'Unable to create swap. Please try again later.', statusColor: 'error', showQr: false});
+                    return;
+                }
                 // id: "U8InKl"
                 // invoice: "lnbc2165060n1pskemqapp54l6hpdcs0t4dpreuwzym9wsqha7f7ra4jll4aywzes8l5h5thq8qdql2djkuepqw3hjq565tqsxzerywfjhxuccqzylxqrrsssp57963jvgs274sk6vkcc5eak0huahhk823000ha9np8fa826fnz38s9qyyssqgdzfrcy6yv0jp564g5tq86nwvfxchenxg7lc48jjaxm34aq0x8ejc5qnwmmtmdrml62n8sh8xu62hmhumrwwlewmyk9yu7c9n5ylkxsp84ryjz"
                 // lockupAddress: "sp2507vnqzc9vbxm7x7kb4sf4qjdjrswhg4v39wpy.stxswap_v7"
                 // onchainAmount: 4950136866
                 // refundAddress: "sp13r6d5p5tye71d81gzqwsd9pgqmqqn54a2yt3by"
                 // timeoutBlockHeight: 34313
-                this.setState({swapId: res.id, invoice: res.invoice, paymentLink: `lightning:${res.invoice}`, swapObj: res, showQr: true});
+                this.setState({swapId: res.id, invoice: res.invoice.toUpperCase(), paymentLink: `lightning:${res.invoice}`, swapObj: res, showQr: true});
                 this.listenswap();
             }).catch(e => {
                 console.log(`createswap error: `, e);
+                this.setState({showLoading: false, showStatus: true, swapStatus: 'Unable to create swap. Please try again later.', statusColor: 'error', showQr: false});
             });  
     }
     listenswap = () => {
         var thisthing = this;
+        // console.log("listenswap state: ", this.state);
         var stream = new EventSource(apiUrl + '/streamswapstatus?id=' + this.state.swapId);
 
         stream.onmessage = function(event) {
@@ -413,16 +470,20 @@ class Widget extends React.Component {
             switch (data.status) {
                 case "transaction.failed":
                     thisthing.setState({showLoading: false, showStatus: true, swapStatus: 'Swap failed. Please try again later.', statusColor: 'error', showQr: false});
+                    stream.close();
                     break;
 
                 case "transaction.confirmed":
-                    thisthing.setState({showLoading: false, showStatus: true, swapStatus: 'Funds locked. Please click to claim.', statusColor: 'success', showButton: true, showQr: false});
+                    thisthing.setState({showLoading: false, showStatus: true, swapStatus: 'Funds are locked in the swap contract. Ready to claim.', statusColor: 'success', showButton: true, showQr: false});
                     break;
 
                 case "transaction.claimed":
-                    thisthing.setState({showLoading: false, showStatus: true, swapStatus: 'Claim successful.', statusColor: 'success'});
+                    thisthing.setState({showLoading: false, showStatus: true, swapStatus: 'Claim successful 🚀', statusColor: 'success', showButton: false, showComplete: true,});
                     break;
 
+                case "invoice.settled":
+                    thisthing.setState({showLoading: false, showStatus: true, swapStatus: 'Claim successful 🚀', statusColor: 'success', showButton: false, showComplete: true,});
+                    break;
                 default:
                     break;
             }
@@ -432,11 +493,13 @@ class Widget extends React.Component {
     }
 
     claimStx = async() => {      
+        let thisthing = this;
+
         this.setState({buttonLoading: true,});
         console.log("swapObj: ", this.state.swapObj);
         let contractAddress = this.state.swapObj.lockupAddress.split(".")[0].toUpperCase();
         let contractName = this.state.swapObj.lockupAddress.split(".")[1]
-        console.log("claimStx ", contractAddress, contractName)
+        // console.log("claimStx ", contractAddress, contractName)
       
         let preimage = this.state.preimage;
         let amount = this.state.swapObj.onchainAmount;
@@ -444,12 +507,12 @@ class Widget extends React.Component {
       
         console.log(`Claiming ${amount} Stx with preimage ${preimage} and timelock ${timeLock}`);
       
-        console.log("amount, decimalamount: ", amount)
+        // console.log("amount, decimalamount: ", amount)
         let smallamount = parseInt(amount / 100) + 1
-        console.log("smallamount: " + smallamount)
+        // console.log("smallamount: " + smallamount)
       
         let swapamount = smallamount.toString(16).split(".")[0] + "";
-        let postConditionAmount = Math.ceil(parseInt(smallamount) / 100);
+        let postConditionAmount = Math.ceil(parseInt(smallamount));
 
         const postConditionAddress = contractAddress;
         const postConditionCode = FungibleConditionCode.LessEqual;
@@ -462,12 +525,12 @@ class Widget extends React.Component {
           )
         ];
       
-        console.log("postConditions: " + contractAddress, contractName, postConditionCode, postConditionAmount)
+        // console.log("postConditions: " + contractAddress, contractName, postConditionCode, postConditionAmount)
       
       
         let paddedamount = swapamount.padStart(32, "0");
         let paddedtimelock = timeLock.toString(16).padStart(32, "0");
-        console.log("amount, timelock ", smallamount, swapamount, paddedamount, paddedtimelock);
+        // console.log("amount, timelock ", smallamount, swapamount, paddedamount, paddedtimelock);
       
         // (claimStx (preimage (buff 32)) (amount (buff 16)) (claimAddress (buff 42)) (refundAddress (buff 42)) (timelock (buff 16)))
         const functionArgs = [
@@ -484,17 +547,18 @@ class Widget extends React.Component {
           functionArgs: functionArgs,
           // validateWithAbi: true,
           network: activeNetwork,
-          postConditionMode: PostConditionMode.Allow,
-          // postConditions,
+        //   postConditionMode: PostConditionMode.Allow,
+          postConditions,
           // anchorMode: AnchorMode.Any,
           onFinish: data => {
-            console.log('Stacks claim onFinish:', JSON.stringify(data));
+            console.log('Stacks claim onFinish:', data);
           },
           onCancel: data => {
-            console.log('Stacks claim onCancel:', JSON.stringify(data));   
+            console.log('Stacks claim onCancel:', data);   
+            thisthing.setState({buttonLoading: false});
           }
         };
-        // await openContractCall(txOptions);
+        await openContractCall(txOptions);
     }
 
 };
