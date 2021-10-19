d2a24b674d5c041de9da79cfc3785f4ece3c1e3c
pseudozach <zozsoy@gmail.com>
2021-10-19 09:49:07 -0700

    correct mocknet demo

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ function getConfig() {
     return {
         name: "LNSwap-Widget",
         // apiUrl: "https://lnswap.org:9001",
-        apiUrl: "https://3999-amethyst-termite-v3th68zo.ws-us17.gitpod.io",
+        apiUrl: "https://9001-amethyst-termite-v3th68zo.ws-us17.gitpod.io",
         pairId: "BTC/STX"
     }
 }
