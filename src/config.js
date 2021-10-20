7d856efdde8c1f1a8f0011d85661f07172f65b04
pseudozach <z@pseudozach.com>
2021-10-20 15:14:54 -0700

    change to gitpod

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ function getConfig() {
     return {
         name: "LNSwap-Widget",
         // apiUrl: "https://lnswap.org:9001",
-        apiUrl: "https://9001-amethyst-termite-v3th68zo.ws-us17.gitpod.io",
+        apiUrl: "https://9001-gray-skunk-peg9m9q7.ws-us17.gitpod.io",
         pairId: "BTC/STX"
     }
 }
