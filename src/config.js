f5c3818de1b4a1141689b61c426cae9cb2593571
pseudozach <z@pseudozach.com>
2022-03-02 15:14:36 -0800

    fix production config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 function getConfig() {
     return {
         name: "LNSwap-Widget",
-        // apiUrl: "https://api.lnswap.org:9002",
+        apiUrl: "https://api.lnswap.org:9002",
         // apiUrl: "https://9002-pseudozach-lnstxbridge-6ldqjy5d9y8.ws-us27.gitpod.io",
-        apiUrl: "http://localhost:9002",
+        // apiUrl: "http://localhost:9002",
         mocknetUrl: "https://localhost:3999",
         // mocknetUrl: "https://3999-pseudozach-lnstxbridge-ymm436d4pnn.ws-us33.gitpod.io",
         pairId: "BTC/STX",
