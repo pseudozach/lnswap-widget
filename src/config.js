6184b5083604a0d6af6f1be0a3139f6df7ed2e9d
pseudozach <zozsoy@gmail.com>
2022-03-19 04:40:59 +0000

    bump triggerswap version to 5

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 function getConfig() {
     return {
         name: "LNSwap-Widget",
-        apiUrl: "https://api.lnswap.org:9007",
+        // apiUrl: "https://api.lnswap.org:9007",
         // apiUrl: "https://9002-pseudozach-lnstxbridge-6ldqjy5d9y8.ws-us27.gitpod.io",
-        // apiUrl: "http://localhost:9002",
+        apiUrl: "http://localhost:9007",
         mocknetUrl: "http://localhost:3999",
         // mocknetUrl: "https://3999-pseudozach-lnstxbridge-6ldqjy5d9y8.ws-us27.gitpod.io",
         pairId: "BTC/STX",
-        triggerContractName: 'triggerswap-v4',
+        triggerContractName: 'triggerswap-v5',
     }
 }
 
