1bfbe290b1019770a990cdfaa3d2972654a7c6f1
pseudozach <zozsoy@gmail.com>
2022-03-21 06:54:37 +0000

    make aggregator api default

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ function getConfig() {
     return {
         name: "LNSwap-Widget",
         apiUrl: "https://api.lnswap.org:9007",
-        // apiUrl: "https://9002-pseudozach-lnstxbridge-6ldqjy5d9y8.ws-us27.gitpod.io",
+        // apiUrl: "https://9002-pseudozach-lnstxbridge-6zdqjy5d9y8.ws-us27.gitpod.io",
         // apiUrl: "http://localhost:9007",
         mocknetUrl: "http://localhost:3999",
         // mocknetUrl: "https://3999-pseudozach-lnstxbridge-6ldqjy5d9y8.ws-us27.gitpod.io",
